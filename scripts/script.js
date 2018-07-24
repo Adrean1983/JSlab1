89422d1a92d08df1a5dcde58d3b361e95bda9100
Adrean1983 <adrean_@hotmail.com>
2018-07-24 17:06:50 -0400

    JS Lab1 Part 2 Submission

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,45 @@
 "use strict";
-
-let play = prompt("Do you want to play? Yes or no?");
-let username;
-let grantDeaths = 0;    
-let grantHealth = 10;
-let userHealth = 40;
-let grantWins = 0; 
-let userWins = 0;
-
-
-if (play.toLowerCase() == "yes") {
-    username = prompt("What is your name?");
-
-    while (userHealth > 0 && grantDeaths !== 3) 
-    {
-        (userHealth -= Math.floor(Math.random()*2)+1);
-        console.log(`${username} has ${userHealth} health`);
-        (grantHealth -= Math.floor(Math.random()*2) +1);
-        console.log (`Grant the mighty chicken has ${grantHealth}`)
-        //these lines below are optional. Separate each turn.
-        if (userHealth > 30) {
-            alert("You're doing great. Keep going!");
-        } else if (userHealth > 20) {
-            alert(`Below 30 health. Watch Out ${username}!`);
-        } else {
-            alert(`You're at ${userHealth}. You can do it.`);
-        }
-        //
-        if (grantHealth < 1) {  
-            grantDeaths++;
-            grantHealth = 10;
-        }
-        } if (grantDeaths === 3) {
-            console.log("You win")
-            userWins++; // I could console.log one user win. But couldn't restart game without refreshing page.
-            console.log(userWins);
-            // alert("you win! Play again?") --I tried to use these two lines to alert player who won. And to reload. But it did not console log score.
-            // document.location.reload();
-        } if (userHealth < 1) {
-            console.log("Grant Wins") 
-            grantWins++;
-            console.log(grantWins);
-            // alert("You lose!");
-            // document.location.reload();
+startGame();
+function startGame() {
+    let play = prompt("Do you want to play? Yes or no?");
+    if (play.toLowerCase() == "yes") {
+        let username = prompt("What is your name?");
+        startCombat(username);
+    } else {
+        alert("Maybe next time");
     }
-} else if (play == "no"){
-    console.log("Maybe next time");
-} else {
-    console.log("Yes or No?");
-}    
+}
+function startCombat(username) {
+    let wins = 0;    
+    let grantHealth = 10;
+    let userHealth = 40;
+
+    while (grantHealth > 0) {
+        let options = prompt("Would you like to 'attack' or 'quit'");
+        if (options.toLowerCase() === "attack") { 
+            userHealth -= getDamage();
+            console.log(`${username} has ${userHealth} health`);
+            grantHealth -= getDamage();
+            console.log (`Grant the mighty chicken has ${grantHealth}`)
+            if (wins === 3) {
+            console.log(`You win ${username}!`);
+            break;
+            }
+            else if  (userHealth <= 0) {
+                console.log("Grant Wins");
+                break; 
+            } else if (grantHealth <= 0) {  
+                wins++;
+                grantHealth = 10;
+                console.log(`You beat Grant ${wins} times!`);
+            }
+        } else if (options.toLowerCase() === "quit") {
+            alert("Lets play another time");
+            break;
+        }    
+    }
+}
+function getDamage() {
+    return Math.floor(Math.random()*5)+1;
+}
+
